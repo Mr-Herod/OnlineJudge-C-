eb30f06bde18906a4630e939c3b6be8bfbc820c2
Mr-Herod <48973275+Mr-Herod@users.noreply.github.com>
2020-04-22 14:54:02 +0800

    2020/04/22 新增头文件   定义了网络数据传输的接口
    socket_init()初始化
    send_data发送数据
    recv_data接收数据
    socket_close()关闭套接字

---
+++ 客户端/datatrans.h
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <WinSock2.h>
+//#pragma comment(lib, "libws2_32.a")
+using namespace std;
+
+
+SOCKET ScoketClient;
+
+int socket_init(int port,string addr);
+int send_data(string str);
+string recv_data(string opt);
+void socket_close();
+
+int socket_init(int port,string addr)
+{
+    WSADATA Wsd;
+    if(WSAStartup(MAKEWORD(1,1),&Wsd) != 0)
+    {
+        cout << "服务器连接失败：初始化套接字动态库失败" << endl;
+        return -1;
+    }
+    ScoketClient = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    if (ScoketClient == INVALID_SOCKET)
+    {
+        cout << "服务器连接失败：创建套接字失败" << endl;
+        WSACleanup();
+        return -1;
+    }
+    SOCKADDR_IN ServerAddr;
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_port = htons(port);
+    ServerAddr.sin_addr.S_un.S_addr = inet_addr((char*)addr.data());
+    int RetVal = connect(ScoketClient, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
+    if (RetVal == SOCKET_ERROR)
+    {
+        cout << "链接服务器失败" << endl;
+        socket_close();
+        return -1;
+    }
+    cout<<"服务器连接成功"<<endl;
+
+}
+
+
+int send_data(string str)
+{
+    int RetVal = send(ScoketClient, (char*)str.data(), str.size() ,0);
+    if (RetVal == SOCKET_ERROR)
+    {
+        cout << "发送数据失败" << endl;
+        socket_close();
+        return -1;
+    }
+    return 1;
+}
+
+string recv_data(string opt)
+{
+    //send(ScoketClient, (char*)opt.data(), opt.size() ,0);
+    char RECVBuff[10024];
+    memset(RECVBuff,0,sizeof(RECVBuff));
+    recv(ScoketClient,RECVBuff,10024, 0);
+    return (RECVBuff);
+}
+
+void socket_close()
+{
+    closesocket(ScoketClient);
+    WSACleanup();
+}
